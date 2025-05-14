e3524faca8da39c8626b5eb6431fcd1dffc9843f
Nahuel De Fazio <ndefazio@finnegans.com.ar>
2025-05-13 22:25:30 -0300

    Cambios en next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
   output: 'export',
-  trailingSlash: true,
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ];
-  },
-  webpack: (config) => {
-    config.experiments = config.experiments || {};
-    config.experiments.topLevelAwait = true;
-    return config;
-  }
 };
 
 module.exports = nextConfig;
